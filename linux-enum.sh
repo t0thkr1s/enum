2872295021364befac6b10f11edb96005480d08c
Kristof Toth <tothkrisi@gmail.com>
2019-08-12 22:43:21 +0200

    enable interpretation of backslash escapes

--- linux-enum.sh
+++ linux-enum.sh
@@ -14,22 +14,22 @@ system_information() {
     kernel=`cat /proc/version 2>/dev/null`
 
     if [ "$kernel" ]; then
-        echo "${GREEN}[ + ] Kernel information:${RST}\n$kernel\n"
+        echo -e "${GREEN}[ + ] Kernel information:${RST}\n$kernel\n"
     else
         uname=`uname -a 2>/dev/null`
         if [ "$uname" ]; then
-            echo "${GREEN}[ + ] Kernel information:${RST}\n$uname\n"
+            echo -e "${GREEN}[ + ] Kernel information:${RST}\n$uname\n"
         fi
     fi
 
     release=`cat /etc/*-release | grep DISTRIB 2>/dev/null`
     if [ "$release" ]; then
-        echo "${GREEN}[ + ] Distribution information:${RST}\n$release\n"
+        echo -e "${GREEN}[ + ] Distribution information:${RST}\n$release\n"
     fi
     
     glibc_version=`ldd --version | head -n 1 2>/dev/null`
     if [ "$glibc_version" ]; then
-        echo "${GREEN}[ + ] GNU C library information:${RST}\n$glibc_version\n"
+        echo -e "${GREEN}[ + ] GNU C library information:${RST}\n$glibc_version\n"
     fi
 
 }
@@ -38,31 +38,31 @@ user_information() {
 
     user_information=`id 2>/dev/null`
     if [ "$user_information" ]; then
-        echo "${GREEN}[ + ] Current user & group information:${RST}\n$user_information\n" 
+        echo -e "${GREEN}[ + ] Current user & group information:${RST}\n$user_information\n" 
     fi
 
     previously_logged_in_users=`lastlog 2>/dev/null | grep -v "Never" 2>/dev/null`
     if [ "$previously_logged_in_users" ]; then
-        echo "${GREEN}[ + ] Previously logged in users:${RST}\n$previously_logged_in_users\n"
+        echo -e "${GREEN}[ + ] Previously logged in users:${RST}\n$previously_logged_in_users\n"
     fi
 
     currently_logged_in_users=`w 2>/dev/null`
     if [ "$currently_logged_in_users" ]; then
-        echo "${GREEN}[ + ] Currently logged in users:\e[00m\n$currently_logged_in_users\n"
+        echo -e "${GREEN}[ + ] Currently logged in users:\e[00m\n$currently_logged_in_users\n"
     fi
     
     passwd_file=`cat /etc/passwd 2>/dev/null`
     if [ "$passwd_file" ]; then
-        echo "${GREEN}[ + ] Contents of /etc/passwd:${RST}\n$passwd_file\n"
+        echo -e "${GREEN}[ + ] Contents of /etc/passwd:${RST}\n$passwd_file\n"
     else
-        echo "${RED}[ - ] The /etc/passwd file is not readable!\n"
+        echo -e "${RED}[ - ] The /etc/passwd file is not readable!\n"
     fi
 
     shadow_file=`cat /etc/shadow 2>/dev/null`
     if [ "$shadow_file" ]; then
-        echo "${GREEN}[ + ] Contents of /etc/shadow:${RST}\n$shadow_file\n"
+        echo -e "${GREEN}[ + ] Contents of /etc/shadow:${RST}\n$shadow_file\n"
     else
-        echo "${RED}[ - ] The /etc/shadow file is not readable!\n"
+        echo -e "${RED}[ - ] The /etc/shadow file is not readable!\n"
     fi
 
 }
