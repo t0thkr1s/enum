191a4402b3d9b2a61ade7159703b9c757da0b3cd
Kristóf Tóth <t0thkr1s@icloud.com>
2020-01-20 22:57:31 +0100

    update format and code refactor

--- linux-enum.sh
+++ linux-enum.sh
@@ -5,74 +5,89 @@
 # Author         : @t0thkr1s
 # Version        : 1.0
 
-GREEN='\033[0;32m'
-RST='\033[0m'
+banner="""
+   __   _
+  / /  (_)  ___  __ __ __ __ ____ ___   ___  __ __  __ _
+ / /  / /  / _ \/ // / \ \ //___// -_) / _ \/ // / /  ' \\
+/_/  /_/  /_//_/\_,_/ /_\_\      \__/ /_//_/\_,_/ /_/_/_/
+"""
+
+header="\n\e[1;97m---------- [ \e[1;96m %s \e[1;97m ] ----------\e[0m\n\n"
+marker="\e[1;97m[ \e[1;92m+ \e[1;97m] %s \e[0m"
 
 system_information() {
 
     kernel=$(cat /proc/version 2>/dev/null)
-
     if [ "$kernel" ]; then
-        echo -e "${GREEN}[ + ] Kernel information:${RST}\n$kernel\n"
+        printf "${header}" "KERNEL INFORMATION"
+        echo "$kernel"
     else
         uname=$(uname -a 2>/dev/null)
         if [ "$uname" ]; then
-            echo -e "${GREEN}[ + ] Kernel information:${RST}\n$uname\n"
+            printf "${header}" "KERNEL INFORMATION"
+            echo "$uname"
         fi
     fi
 
     release=$(cat /etc/*-release | grep DISTRIB 2>/dev/null)
     if [ "$release" ]; then
-        echo -e "${GREEN}[ + ] Distribution information:${RST}\n$release\n"
+        printf "${header}" "DISTRIBUTION INFORMATION"
+        echo "$release"
     fi
-
 }
 
 user_information() {
 
     user_information=$(id 2>/dev/null)
     if [ "$user_information" ]; then
-        echo -e "${GREEN}[ + ] Current user & group information:${RST}\n$user_information\n"
+        printf "${header}" "USER & GROUP INFORMATION"
+        echo "$user_information"
     fi
 
     previously_logged_in_users=$(lastlog 2>/dev/null | grep -v "Never" 2>/dev/null)
     if [ "$previously_logged_in_users" ]; then
-        echo -e "${GREEN}[ + ] Previously logged in users:${RST}\n$previously_logged_in_users\n"
+        printf "${header}" "PREVIOSULY LOGGED IN USERS"
+        echo "$previously_logged_in_users"
     fi
 
     currently_logged_in_users=$(w 2>/dev/null)
     if [ "$currently_logged_in_users" ]; then
-        echo -e "${GREEN}[ + ] Currently logged in users:\e[00m\n$currently_logged_in_users\n"
+        printf "${header}" "CURRENTLY LOGGED IN USERS"
+        echo "$currently_logged_in_users"
     fi
 
     passwd_file=$(cat /etc/passwd 2>/dev/null)
     if [ "$passwd_file" ]; then
-        echo -e "${GREEN}[ + ] Contents of /etc/passwd:${RST}\n$passwd_file\n"
+        printf "${header}" "CONTENT OF THE PASSWD FILE"
+        echo "$passwd_file"
     fi
 
     shadow_file=$(cat /etc/shadow 2>/dev/null)
     if [ "$shadow_file" ]; then
-        echo -e "${GREEN}[ + ] Contents of /etc/shadow:${RST}\n$shadow_file\n"
+        printf "${header}" "CONTENT OF THE SHADOW FILE"
+        echo "$shadow_file"
     fi
 
     sudoers_file=$(grep -v -e '^$' /etc/sudoers 2>/dev/null | grep -v "#" 2>/dev/null)
     if [ "$sudoers_file" ]; then
-        echo -e "${GREEN}[ + ] Contents of /etc/sudoers:${RST}\n$sudoers_file\n"
+        printf "${header}" "CONTENT OF THE SUDOERS FILE"
+        echo "$sudoers_file"
     fi
 
     root_directory_permissions=$(ls -ahl /root/ 2>/dev/null)
     if [ "$root_directory_permissions" ]; then
-        echo -e "${GREEN}[ + ] The /root directory is readable!${RST}\n$root_directory_permissions\n"
+        printf "$marker" "The /root directory is readable!"
     fi
 
     home_directory_permissions=$(ls -ahl /home/ 2>/dev/null)
     if [ "$home_directory_permissions" ]; then
-        echo -e "${GREEN}[ + ] Home directory permissions:${RST}\n$home_directory_permissions\n"
+        printf "${header}" "HOME DIRECTORY PERMISSIONS"
+        echo "$home_directory_permissions"
     fi
 
     sudo_without_password=$(echo '' | sudo -S -l -k 2>/dev/null)
     if [ "$sudo_without_password" ]; then
-        echo -e "${GREEN}[ + ] We can sudo without a password!!!:${RST}\n"
+        printf "$marker" "We can sudo without a password!"
     fi
 
 }
@@ -81,17 +96,19 @@ environmental_information() {
 
     enviroment_variables=$(env 2>/dev/null | grep -v 'LS_COLORS' 2>/dev/null)
     if [ "$enviroment_variables" ]; then
-        echo -e "${GREEN}[ + ] Environment information:${RST}\n$enviroment_variables\n"
+        printf "${header}" "ENVIRONMENT VARIABLES"
+        echo "$enviroment_variables"
     fi
 
     selinux_status=$(sestatus 2>/dev/null)
     if [ "$selinux_status" ]; then
-        echo -e "${GREEN}[ + ] SELinux seems to be present:${RST}\n$selinux_status\n"
+        printf "$marker" "SELinux seems to be present!"
     fi
 
     available_shells=$(tail -n +2 /etc/shells 2>/dev/null)
     if [ "$available_shells" ]; then
-        echo -e "${GREEN}[ + ] Available shells:${RST}\n$available_shells\n"
+        printf "${header}" "AVAILABLE SHELLS"
+        echo "$available_shells"
     fi
 
 }
@@ -100,7 +117,8 @@ service_information() {
 
     running_processes=$(ps aux 2>/dev/null)
     if [ "$running_processes" ]; then
-        echo -e "${GREEN}[ + ] Running processes:${RST}\n$running_processes\n"
+        printf "${header}" "RUNNING PROCESSES"
+        echo "$running_processes"
     fi
 
 }
@@ -109,32 +127,36 @@ software_information() {
 
     glibc_version=$(ldd --version | head -n 1 2>/dev/null)
     if [ "$glibc_version" ]; then
-        echo -e "${GREEN}[ + ] GNU C library information:${RST}\n$glibc_version\n"
+        printf "${header}" "GNU C LIBRARY INFORMATION"
+        echo "$glibc_version"
     fi
 
     sudo_version=$(sudo -V 2>/dev/null | grep "Sudo version" 2>/dev/null)
     if [ "$sudo_version" ]; then
-        echo -e "${GREEN}[ + ] Sudo version:${RST}\n$sudo_version\n"
+        printf "${header}" "SUDO VERSION"
+        echo "$sudo_version"
     fi
 
     mysql_version=$(mysql --version 2>/dev/null)
     if [ "$mysql_version" ]; then
-        echo -e "${GREEN}[ + ] Mysql version:${RST}\n$mysql_version\n"
+        printf "${header}" "MYSQL VERSION"
+        echo "$mysql_version"
     fi
 
     mysql_default=$(mysqladmin -uroot -proot version 2>/dev/null)
     if [ "$mysql_default" ]; then
-        echo -e "${GREEN}[ + ] We can connect with the default (root:root) credentials!${RST}\n$mysql_default\n"
+        printf "$marker" "Default MySQL credentials (root:root) are being used!"
     fi
 
     mysql_nopassword=$(mysqladmin -uroot version 2>/dev/null)
     if [ "$mysql_nopassword" ]; then
-        echo -e "${GREEN}[ + ] We can connect as 'root' without a password!${RST}\n$mysql_nopassword\n"
+        printf "$marker" "The MySQL root user doesn't require a password!"
     fi
 
     postgres_version=$(psql -V 2>/dev/null)
     if [ "$postgres_version" ]; then
-        echo -e "${GREEN}[ + ] Postgres version:${RST}\n$postgres_version\n"
+        printf "${header}" "POSTGRESQL VERSION"
+        echo "$postgres_version"
     fi
 
     apache_version=$(
@@ -142,12 +164,14 @@ software_information() {
         httpd -v 2>/dev/null
     )
     if [ "$apache_version" ]; then
-        echo -e "${GREEN}[ + ] Apache version:${RST}\n$apache_version\n"
+        printf "${header}" "APACHE VERSION"
+        echo "$apache_version"
     fi
 
     useful_software=$(command -v nmap aws nc ncat netcat nc.traditional wget curl ping gcc g++ make gdb base64 socat python python2 python3 python2.7 python2.6 python3.6 python3.7 perl php ruby xterm doas sudo fetch 2>/dev/null)
     if [ "$useful_software" ]; then
-        echo -e "${GREEN}[ + ] Useful software:${RST}\n$useful_software\n"
+        printf "${header}" "USEFUL SOFTWARE"
+        echo "$useful_software"
     fi
 
 }
@@ -156,47 +180,44 @@ file_information() {
 
     suid_files=$(find / -perm -4000 -type f -exec ls -la {} \; 2>/dev/null)
     if [ "$suid_files" ]; then
-        echo -e "${GREEN}[ + ] SUID files:${RST}\n$suid_files\n"
-    fi
-
-    ssh_keys=$(grep -rl "PRIVATE KEY-----" /home 2>/dev/null)
-    if [ "$ssh_keys" ]; then
-        echo -e "${GREEN}[ + ] Private SSH keys found:${RST}\n$ssh_keys\n"
-    fi
-
-    aws_keys=$(grep -rli "aws_secret_access_key" /home 2>/dev/null)
-    if [ "$aws_keys" ]; then
-        echo -e "${GREEN}[ + ] AWS secret access keys found:${RST}\n$aws_keys\n"
+        printf "${header}" "SUID FILES"
+        echo "$suid_files"
     fi
 
     git_credentials=$(find / -name ".git-credentials" 2>/dev/null)
     if [ "$git_credentials" ]; then
-        echo -e "${GREEN}[ + ] Git credentials found:${RST}\n$git_credentials\n"
+        printf "${header}" "GIT CREDENTIALS"
+        echo "$git_credentials"
     fi
 
-    world_writable_files=$(find / ! -path "*/proc/*" ! -path "/sys/*" -perm -2 -type f -exec ls -la {} \; 2>/dev/null)
+    world_writable_files=$(find / ! -path "*/proc/*" ! -path "/sys/*" ! -path "/var/lib/*" -perm -2 -type f -exec ls -la {} \; 2>/dev/null)
     if [ "$world_writable_files" ]; then
-        echo -e "${GREEN}[ + ] World-writable files (excluding /proc and /sys):${RST}\n$world_writable_files\n"
+        printf "${header}" "WORLD-WRITABLE FILES"
+        echo "$world_writable_files"
     fi
 
     mails=$(ls -la /var/mail 2>/dev/null)
-    if [ "$mails" ]; then
-        echo -e "${GREEN}[ + ] Listing mails in /var/mail:${RST}\n$mails\n"
+    if [ "$(ls -A /var/mail)" ] && [ "$mails" ]; then
+        printf "${header}" "MAILS"
+        echo "$mails"
     fi
 
     gpg_keys=$(gpg --list-keys 2>/dev/null)
     if [ "$gpg_keys" ]; then
-        echo -e "${GREEN}[ + ] GPG keys:${RST}\n$gpg_keys\n"
+        printf "${header}" "GPG KEYS"
+        echo "$gpg_keys"
     fi
 
     ovpn_files=$(find / -name "*.ovpn" 2>/dev/null)
     if [ "$ovpn_files" ]; then
-        echo -e "${GREEN}[ + ] OVPN files:${RST}\n$ovpn_files\n"
+        printf "${header}" "OVPN FILES"
+        echo "$ovpn_files"
     fi
 
-    vnc_directories=$(find /home / -type d -name .vnc 2>/dev/null)
+    vnc_directories=$(find / -type d -name .vnc 2>/dev/null)
     if [ "$vnc_directories" ]; then
-        echo -e "${GREEN}[ + ] VNC directories:${RST}\n$vnc_directories\n"
+        printf "${header}" "VNC DIRECTORIES"
+        echo "$vnc_directories"
     fi
 
 }
@@ -208,17 +229,19 @@ container_information() {
         find / -name "*dockerenv*" -exec ls -la {} \; 2>/dev/null
     )
     if [ "$docker_container" ]; then
-        echo -e "${GREEN}[ + ] Looks like we are in a Docker container:${RST}\n$docker_container\n"
+        printf "$marker" "It looks like we're in a Docker container!"
     fi
 
     lxc_container=$(grep -qa container=lxc /proc/1/environ 2>/dev/null)
     if [ "$lxc_container" ]; then
-        echo -e "${GREEN}[ + ] Looks like we are in a LXC container:${RST}\n$lxc_container\n"
+        printf "$marker" "It looks like we're in a LXC container!"
     fi
 
 }
 
 main() {
+    printf "\033c"
+    printf "$banner"
     system_information
     user_information
     environmental_information
